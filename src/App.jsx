8ca6c6b5c1fa981a48390c011f5b066b456d9d48
TechyBuilder8 <techybuilder8545@gmail.com>
2025-09-15 13:36:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-5xl bg-pink-200 text-center py-4'>App is running</div>
+  )
+}
+
+export default App
